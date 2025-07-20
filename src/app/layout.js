7269e6d63e906c0e498bd1dd4302cdfc78bc087a
MohamedMostafa-1 <mohamedmostafajalal@gmail.com>
2025-07-20 03:32:33 -0700

    Fix import path for LoadingLayout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import LoadingLayout from "@/components/LoadingLayout";
-
+import LoadingLayout from '@/components/LoadingLayout';
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
