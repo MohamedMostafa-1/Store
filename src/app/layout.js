8b677d34f84935064e0ca2ff2aefe6b5351bbfa0
MohamedMostafa-1 <mohamedmostafajalal@gmail.com>
2025-07-20 03:51:33 -0700

    Fix Components import path case sensitivity

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import LoadingLayout from '@/components/LoadingLayout';
+import LoadingLayout from '@/Components/LoadingLayout';
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
